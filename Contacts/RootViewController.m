87c80baf9a7d3ee8e437560a17300fa74abd2828
fengliya <995736140@qq.com>
2016-01-28 14:32:44 +0800

    增加ContactsUI

--- Contacts/RootViewController.m
+++ Contacts/RootViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import <Contacts/Contacts.h>
+#import <ContactsUI/ContactsUI.h>
 #import "RootViewController.h"
 #import "FriendDetailViewController.h"
 #import "FriendCell.h"
 
 static NSString *cellIdentifier = @"friendCell";
 
-@interface RootViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
+@interface RootViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, CNContactPickerDelegate>
 
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) NSMutableArray *allFriends;
@@ -87,14 +88,25 @@ static NSString *cellIdentifier = @"friendCell";
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
     CNContact *friend = self.allFriends[indexPath.row];
-    [self showFriendDetailWithFriend:friend];
+    
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"展示方式" message:@"请选择一种展示方式。" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *pickAction = [UIAlertAction actionWithTitle:@"contact VC" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self showFriendDetailByVC:friend];
+    }];
+    
+    UIAlertAction *userAction = [UIAlertAction actionWithTitle:@"自定义" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self showFriendDetailWithFriend:friend];
+    }];
+    [alertVC addAction:pickAction];
+    [alertVC addAction:userAction];
+    [self.navigationController presentViewController:alertVC animated:YES completion:nil];
 }
 
 - (void)fetchAllFriends
 {
     [self.allFriends removeAllObjects];
     CNContactStore *store = [[CNContactStore alloc] init];
-    CNContactFetchRequest *request = [[CNContactFetchRequest alloc] initWithKeysToFetch:@[CNContactGivenNameKey,CNContactFamilyNameKey, CNContactPhoneNumbersKey, CNContactEmailAddressesKey]];
+    CNContactFetchRequest *request = [[CNContactFetchRequest alloc] initWithKeysToFetch:@[CNContactIdentifierKey,CNContactGivenNameKey,CNContactFamilyNameKey, CNContactPhoneNumbersKey, CNContactEmailAddressesKey,[CNContactViewController descriptorForRequiredKeys]]];
     typeof(&*self) __weak weakSelf = self;
     [store enumerateContactsWithFetchRequest:request error:nil usingBlock:^(CNContact * _Nonnull contact, BOOL * _Nonnull stop) {
         [weakSelf.allFriends addObject:contact];
@@ -103,7 +115,29 @@ static NSString *cellIdentifier = @"friendCell";
 
 - (void)addNewFriend
 {
-    [self showFriendDetailWithFriend:nil];
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"添加方式" message:@"请选择一种添加方式。" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *pickAction = [UIAlertAction actionWithTitle:@"picker VC" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self showContactPickerViewController];
+    }];
+    
+    UIAlertAction *userAction = [UIAlertAction actionWithTitle:@"自定义" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self showFriendDetailWithFriend:nil];
+    }];
+    [alertVC addAction:pickAction];
+    [alertVC addAction:userAction];
+    [self.navigationController presentViewController:alertVC animated:YES completion:nil];
+}
+
+- (void)showFriendDetailByVC:(CNContact *)friend
+{
+    NSArray *keysToFatch = @[CNContactIdentifierKey,CNContactGivenNameKey,CNContactFamilyNameKey,CNContactPhoneNumbersKey,CNContactEmailAddressesKey,[CNContactViewController descriptorForRequiredKeys]];
+    if (![friend areKeysAvailable:keysToFatch]) {
+        return;
+    }
+    CNContactViewController *vc = [CNContactViewController viewControllerForContact:friend];
+    vc.allowsEditing = YES;
+    vc.allowsActions = YES;
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 - (void)showFriendDetailWithFriend:(CNContact *)friend
@@ -112,9 +146,22 @@ static NSString *cellIdentifier = @"friendCell";
     FriendDetailViewController *detailVC = [[FriendDetailViewController alloc] init];
     detailVC.contact = friend;
     detailVC.block = ^(BOOL isFinished) {
-        [weakSelf constructFriends]; //此处应该用弱引用
+        [weakSelf constructFriends];
     };
     [self.navigationController pushViewController:detailVC animated:YES];
+}
+
+- (void)showContactPickerViewController
+{
+    CNContactPickerViewController *pickerVC = [CNContactPickerViewController new];
+    pickerVC.delegate = self;
+    [self.navigationController presentViewController:pickerVC animated:YES completion:nil];
+}
+
+- (void)contactPicker:(CNContactPickerViewController *)picker didSelectContact:(CNContact *)contact
+{
+    [self.allFriends addObject:contact];
+    [self.collectionView reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
